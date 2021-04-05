c2f99e084874b37a9fa930fb9fc2c902471b37d3
Emily Y <emilygbs@yahoo.com>
2021-04-05 15:16:03 -0400

    fixed adding cookie to bypass authentication, added handling for no course reviews found

--- webscraping/critical_review_scraper.py
+++ webscraping/critical_review_scraper.py
@@ -7,7 +7,7 @@ import time
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException
 
 from cookies import COOKIES
 
@@ -17,10 +17,32 @@ class CritReviewScraper:
         self.driver = webdriver.Chrome()
         self.driver.get("https://thecriticalreview.org")
 
+        self.driver.delete_cookie("connect.sid")
         # using another file to store cookie values to keep accounts secure
-        self.driver.add_cookie({"name": "connect.sid", "domain": "https://thecriticalreview.org/",
+        self.driver.add_cookie({"name": "connect.sid", "domain": ".thecriticalreview.org",
                                 "value": COOKIES["Critical Review"]})
 
         self.timeout = timeout
         self.filename = filename
         self.courses = []
+
+    def scrape_course(self, course):
+        department = course[:course.index(" ")]
+        course_code = course[course.index(" ") + 1:]
+
+        self.driver.get("https://thecriticalreview.org/search/" + department + "/" + course_code)
+        #soup = BeautifulSoup(self.driver.page_source)
+
+        try:
+            # no reviews for the course
+            no_reviews = self.driver.find_element_by_css_selector("unreturned_banner_message")
+            print("no reviews found")
+            return {}
+        except NoSuchElementException:
+            # TODO: scrape course review
+            return {}
+
+
+filename = input("What file should data be saved to (ex: CritReview_data.csv)?\t")
+scraper = CritReviewScraper(filename)
+scraper.scrape_course("CSCI 0300")
