712d80176739afbca77a1a2ab49e289df8655f74
GarethMansfield <gareth_mansfield@brown.edu>
2021-04-15 05:41:12 -0400

    Removed Fs

--- client/src/components/CourseView.js
+++ client/src/components/CourseView.js
@@ -10,38 +10,36 @@ import {
   Td,
   TableCaption,
   Divider,
-  Button
+  Button,
+  Tag
 } from "@chakra-ui/react"
 
 function CourseView(props) {
+
+    function getCategories() {
+      let result = [];
+      for (let i = 0; i < props.node.categories.length; i++) {
+        if (props.node.categories[i] === 'TRUE') {
+          result.push(['FYS', 'SOPH', 'DIAP', 'WRIT', 'CBLR', 'COEX'][i])
+        }
+      }
+      return result;
+    }
+
     return (
         <div style={{width: props.width, height: props.height}} className="courseView">
             <h1 className="courseTitle">{props.node.id + ": " + props.node.name}</h1>
             <Divider orientation="horizontal" />
             <p className="courseDesc">{props.node.desc}</p>
-            <Table>
-                <Thead>
-                  <Tr>
-                    <Th>FYS</Th>
-                    <Th>SOPH</Th>
-                    <Th>DIAP</Th>
-                    <Th>WRIT</Th>
-                    <Th>CBLR</Th>
-                    <Th>COEX</Th>
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  <Tr>
-                    {
-                        props.node.categories.map((s, index) => {return(<Td key={index} className="categoryTd">{s[0]}</Td>);})
-                    }
-                  </Tr>
+            <div style={{textAlign: 'center'}}>
+              {
+                getCategories().map((x, i) => {
+                  return <Tag key={i} style={{margin: '10px'}} size='lg' borderRadius="full"
+                    variant="solid"
+                    colorScheme="green">{x}</Tag>})
+              }
+            </div>
 
-                <Tr>
-
-                </Tr>
-                </Tbody>
-            </Table>
             <br/>
             {props.annotation &&
               <Button colorScheme="green" style={{display: 'block', margin: 'auto', }}
