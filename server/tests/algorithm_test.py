ed1ed32ebb97dfbd6444d835798c9dcf95797fec
William Sun <william.h.sun.88@gmail.com>
2021-04-16 22:14:12 -0400

    Added more testing

--- server/tests/algorithm_test.py
+++ server/tests/algorithm_test.py
@@ -22,15 +22,6 @@ class TestAlgorithmProperties(unittest.TestCase):
         self.assertEqual(len(self.algorithm.get_recs(["CSCI 0190", "CSCI 0320"], [])), 1)
         self.assertEqual(len(self.algorithm.get_recs(["CSCI 0190", "CSCI 0320"], [], None)), 2603)
 
-class TestAlgorithmResultsProperties(unittest.TestCase):
-
-    def setUp(self):
-        self.text_compare = TextComparison()
-        self.metadata_compare = MetadataComparison(parent_dir + "/data/CritReview_data_v2.csv")
-        self.text_compare.import_saved_similarity(parent_dir + "/data/similarities_v2.csv")
-        self.text_compare.import_department_similarity(parent_dir + "/data/dept_similarities_v2.csv")
-        self.algorithm = Algorithm(self.text_compare, self.metadata_compare)
-
     def test_sub_results(self):
         self.assertEqual(self.algorithm.get_recs(["CSCI 0190", "CSCI 0320"], [], 10), 
                     self.algorithm.get_recs(["CSCI 0190", "CSCI 0320"], [], None)[:10])
@@ -46,6 +37,29 @@ class TestAlgorithmResultsProperties(unittest.TestCase):
                     self.algorithm.get_recs(["CSCI 0190", "CSCI 0320"], ["High enjoyment", "Low difficulty"], 10))
         self.assertNotEqual(self.algorithm.get_recs(["CSCI 0190", "CSCI 0320"], ["High enjoyment", "Low difficulty", "Low time commitment"], 10), 
                     self.algorithm.get_recs(["CSCI 0190", "CSCI 0320"], ["High enjoyment", "Low difficulty"], 10))
+    
+    def test_edge_case_inputs(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.algorithm.get_recs([], ["High enjoyment"], 10)
+        with self.assertRaises(KeyError):
+            self.algorithm.get_recs(["CSCI 0190"], ["high enjoyment"], 10)
+        self.assertEqual(len(self.algorithm.get_recs(["CSCI 0190"], ["High enjoyment"], 10000)), 
+                    len(self.algorithm.get_recs(["CSCI 0190"], ["High enjoyment"], None)))
+
+class TestAlgorithmResultsProperties(unittest.TestCase):
+
+    def setUp(self):
+        self.text_compare = TextComparison()
+        self.metadata_compare = MetadataComparison(parent_dir + "/data/CritReview_data_v2.csv")
+        self.text_compare.import_saved_similarity(parent_dir + "/data/similarities_v2.csv")
+        self.text_compare.import_department_similarity(parent_dir + "/data/dept_similarities_v2.csv")
+        self.algorithm = Algorithm(self.text_compare, self.metadata_compare)
+
+    def test_departments(self):
+        self.assertTrue("CSCI" in [a[0].split(" ")[0] for a in self.algorithm.get_recs(["CSCI 0190", "CSCI 0320", "CSCI 0330"], [], 10)])
+        self.assertTrue("MATH" in [a[0].split(" ")[0] for a in self.algorithm.get_recs(["CSCI 0220", "MATH 0540"], [], 10)])
+        self.assertTrue("VISA" in [a[0].split(" ")[0] for a in self.algorithm.get_recs(["CSCI 0190", "VISA 0100"], [], 10)])
+        self.assertTrue("CSCI" in [a[0].split(" ")[0] for a in self.algorithm.get_recs(["CSCI 0190", "VISA 0100"], [], 10)])
 
 if __name__ == '__main__':
     unittest.main()
